a802343f9ab9755d88e2ae4d9caa0ad83a364290
SamGuido <55506520+SamGuido@users.noreply.github.com>
2019-11-20 17:56:29 +0300

    1-st page

--- src/js/base.js
+++ src/js/base.js
@@ -1,1 +1,31 @@
-console.log('Привет')
+console.log('Hello!')
+
+$(document).ready(function () {
+
+    //Emersion arrow to up scrolling
+
+    $(window).on("scroll", function () {
+
+        if ($(window).scrollTop()) {
+            $('#scrollUp').removeClass('scrollUp_off');
+        } else {
+            $('#scrollUp').addClass('scrollUp_off');
+        }
+
+    });
+
+    //Slow scrolling to an anchor 
+
+    $("#scrollUp").click(function () {
+
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top + "px"
+        }, {
+            duration: 1000,
+            easing: "swing"
+        });
+        return false;
+    });
+
+
+});
